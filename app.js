468d458535a985db6ae26a3f9a41aba2bb47db31
Abhishek Kulkarni <abhishekkul2000@gmail.com>
2022-12-04 18:25:38 +0530

    Fixed company feature

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const btn_feature = document.getElementById("btn-feature");
 const feature_items = document.querySelectorAll(".feature_menu");
 
-// let value = undefined;
-
 btn_feature.addEventListener("click", () => {
   feature_items.forEach((item) => {
+    item.classList.add("active");
+    item.classList.toggle("hide");
+  });
+});
+
+const btn_company = document.getElementById("btn-cmpny");
+const company_items = document.querySelectorAll(".company_menu");
+
+btn_company.addEventListener("click", () => {
+  company_items.forEach((item) => {
     item.classList.add("active");
     item.classList.toggle("hide");
   });
