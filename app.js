c5211e712f017fc045cdc464ca0f48b51f4206a3
Abhishek Kulkarni <abhishekkul2000@gmail.com>
2022-12-12 20:28:30 +0530

    Fixed responsiveness

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const btn_feature = document.getElementById("btn-feature");
 const feature_items = document.querySelectorAll(".feature_menu");
+const arrow_btn_feature = document.getElementById("arrowDown");
+const arrow_btn_company = document.getElementById("arrowdown");
 
 btn_feature.addEventListener("click", () => {
   feature_items.forEach((item) => {
-    item.classList.add("active");
-    item.classList.toggle("hide");
+    item.classList.toggle("active");
+    arrow_btn_feature.classList.toggle("rotate");
   });
 });
 
@@ -13,7 +15,8 @@ const company_items = document.querySelectorAll(".company_menu");
 
 btn_company.addEventListener("click", () => {
   company_items.forEach((item) => {
-    item.classList.add("active");
-    item.classList.toggle("hide");
+    item.classList.toggle("active");
+    arrow_btn_company.classList.toggle("rotate");
   });
 });
+
