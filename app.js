1be9662873001d249fc1ebcd8199185fd78056a5
Abhishek Kulkarni <abhishekkul2000@gmail.com>
2022-12-13 20:14:17 +0530

    buid navbar functionalitry and resposiveness

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const btn_feature = document.getElementById("btn-feature");
 const feature_items = document.querySelectorAll(".feature_menu");
 const arrow_btn_feature = document.getElementById("arrowDown");
 const arrow_btn_company = document.getElementById("arrowdown");
+const btn_company = document.getElementById("btn-cmpny");
+const company_items = document.querySelectorAll(".company_menu");
+const menu_btn = document.getElementById("menu");
 
 btn_feature.addEventListener("click", () => {
   feature_items.forEach((item) => {
@@ -10,9 +13,6 @@ btn_feature.addEventListener("click", () => {
   });
 });
 
-const btn_company = document.getElementById("btn-cmpny");
-const company_items = document.querySelectorAll(".company_menu");
-
 btn_company.addEventListener("click", () => {
   company_items.forEach((item) => {
     item.classList.toggle("active");
@@ -20,3 +20,11 @@ btn_company.addEventListener("click", () => {
   });
 });
 
+menu_btn.addEventListener("click", () => {
+  document.getElementById("container").style.visibility = "visible";
+  menu_btn.classList.toggle("active");
+  menu_btn.style.zIndex = 1;
+  menu_btn.src = "./images/icon-close-menu.svg";
+});
+
+// To Add close functionality and background blurry and darkish.
